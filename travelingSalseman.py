3c9a5ebb0f6edf743253ae35f2d2b483e5d9834e
kctaids <120014444+kctaids@users.noreply.github.com>
2022-12-12 10:00:59 +0530

    Update travelingSalseman.py

--- travelingSalseman.py
+++ travelingSalseman.py
@@ -20,11 +20,8 @@ def tsp(graph, s):
 
     return min_weight
 
-# Driver Code
-if __name__ == "__main__":
 
-	# matrix representation of graph
-	graph = [[0, 10, 15, 20], [10, 0, 35, 25],
-			[15, 35, 0, 30], [20, 25, 30, 0]]
+if __name__ == "__main__":
+	graph = [[0, 10, 15, 20], [10, 0, 35, 25],[15, 35, 0, 30], [20, 25, 30, 0]]
 	s = 0
 	print(tsp(graph, s))
