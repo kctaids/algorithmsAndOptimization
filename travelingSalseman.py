fedf864ed9499f89b8b23d6e90ae1c0542c8231c
kctaids <120014444+kctaids@users.noreply.github.com>
2022-12-12 10:00:31 +0530

    Create travelingSalseman.py

---
+++ travelingSalseman.py
@@ -0,0 +1,30 @@
+from sys import maxsize
+from itertools import permutations
+
+
+def tsp(graph, s):
+    V = len(graph)
+    vertex = [i for i in range(V) if i != s]
+    perms = permutations(vertex)
+
+    min_weight = maxsize
+    for i in perms:
+        curr_weight = 0
+        k = s
+        for j in i:
+            curr_weight += graph[k][j]
+            k = j
+        curr_weight += graph[k][s]
+
+        min_weight = min(curr_weight, min_weight)
+
+    return min_weight
+
+# Driver Code
+if __name__ == "__main__":
+
+	# matrix representation of graph
+	graph = [[0, 10, 15, 20], [10, 0, 35, 25],
+			[15, 35, 0, 30], [20, 25, 30, 0]]
+	s = 0
+	print(tsp(graph, s))
